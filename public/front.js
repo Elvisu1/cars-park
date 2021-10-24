f40d3662dbeecdc31a243add6c64d3785de1ce9f
Elvis <elvinasu@gmail.com>
2021-10-24 12:58:33 +0300

    cars apears

--- public/front.js
+++ public/front.js
@@ -1,14 +1,36 @@
-const url = `http://localhost:3000/car`
+const URL = `http://localhost:3000/car`
+
+//html elements
+const carsListEl = document.querySelector('.cars-list')
 
 const getCars = async () => {
-    const resp = await fetch(url);
+    const resp = await fetch(URL);
     const data = await resp.json();
     console.log('data', data)
+    return data.result;
 };
 
+const renderCars = (arr, dest) => {
+const generatedCars = arr.map(carItem =>{
+    return `
+    <div class="card" style="width: 18rem;">
+            <img src="${carItem.image}" class="card-img-top" alt="...">
+            <div class="card-body">
+                <h5 class="card-title">${carItem.title}</h5>
+                <p class="card-text">${carItem.price} € </p>
+                <button class="btn btn-primary">View</button>
+            </div>
+        </div>
+    `
 
+});
+    dest.innerHTML = generatedCars;
+}
 const init = async () => {
-await getCars();
+const cars = await getCars();
+renderCars(cars, carsListEl);
 };
 
 init();
+
+
--- src/server.js
+++ src/server.js
@@ -5,33 +5,31 @@ const cors = require('cors');
 const mysql = require('mysql2/promise');
 const dbConfig = require('./dbConfig');
 
-//routes
-
-const carsRoute = require('./api/V1/cars')
-
+// routes
+const carRoute = require('./api/V1/cars')
 
 
 const PORT = process.env.SERVER_PORT || 3000;
 
 const app = express();
 
-
-app.use('/car', carsRoute)
 // middleware
 app.use(morgan('common'));
 app.use(cors());
 app.use(express.json());
 
 app.get('/', async (req, res) => {
-   try{
-      const conn = await mysql.createConnection(dbConfig)
-      res.send({msg: 'connected'})
-      await conn.end()
-   } catch (error){
-      console.log('/got error', error.message)
-      res.status(500).send(`something is wrong on port ${PORT}`)
-   };
-   res.send('hello world')
+   try {
+      const conn = await mysql.createConnection(dbConfig);
+      res.send({ msg: 'Connected' });
+      await conn.end();
+   } catch (error) {
+      console.log('/ got error ', error.message);
+      res.status(500).send({ error: 'Sometning went wrong' });
+   }
 });
+
+app.use('/car', carRoute);
+
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
